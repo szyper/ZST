479a0d5e6b2a2a11cd99e0daec5322020de349be
szyper <31780468+szyper@users.noreply.github.com>
2025-09-10 14:19:07 +0200

    string

--- 3Tmp_programowanie_obiektowe/project_1/Program.cs
+++ 3Tmp_programowanie_obiektowe/project_1/Program.cs
@@ -10,7 +10,28 @@ namespace project_1
   {
     static void Main(string[] args)
     {
+      Console.Write("ZST");
+      Console.WriteLine("programowanie");
+      Console.Write("test\n");
       Console.WriteLine("ZST");
+
+      string firstName = "Jan";
+      Console.WriteLine("Imię: " + firstName);
+
+      Console.Write("Podaj nazwisko: ");
+      string lastName = Console.ReadLine();
+      Console.WriteLine("Podane nazwisko z klawiatury: " + lastName);
+
+      int age = int.Parse(Console.ReadLine());
+      if (age >= 18)
+      {
+        Console.WriteLine("Jesteś pełnoletni");
+      }
+      else
+      {
+        Console.WriteLine("Jesteś dzieckiem");
+      }
+
     }
   }
 }
