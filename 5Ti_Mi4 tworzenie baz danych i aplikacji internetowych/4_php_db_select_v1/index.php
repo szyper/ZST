7b073210691a9edb0a6c12bbfdccebd91e07ce8d
szyper <31780468+szyper@users.noreply.github.com>
2025-09-16 17:26:18 +0200

    db connect

---
+++ 5Ti_Mi4 tworzenie baz danych i aplikacji internetowych/4_php_db_select_v1/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        /* table, tr, th, td {
+            border: solid 1px grey;
+            border-collapse: collapse;
+            padding: 10px;
+        } */
+    </style>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php
+    require_once('./config.php');
+
+    function calculateAge($birthDate){
+        $dateOfBirth = new DateTime($birthDate); //data urodzenia
+        $currentDate = new DateTime(); //dzisiejsza data
+        $age = $currentDate->diff($dateOfBirth)->y; //różnica w latach
+        return $age;
+    }
+
+    $mysqli = new mysqli($host, $user, $pass, $db_name);
+
+    if ($mysqli->connect_error) {
+        die("Błąd połączenia: " . $mysqli->connect_error);
+    }
+
+    $sql = "SELECT id, firstName, email, birth_date, gender, description FROM `users`;";
+
+    $result = $mysqli->query($sql);
+
+    echo $result->num_rows."<hr>";
+
+    if ($result->num_rows > 0) {
+        echo "<h2>Lista użytkowników:</h2>";
+        echo "<table>
+        <tr>
+        <th>Id</th>
+        <th>Imię</th>
+        <th>Email</th>
+        <th>Data urodzenia</th>
+        <th>Wiek</th>
+        <th>Płeć</th>
+        <th>Opis</th>
+        </tr>";
+        while($row = $result->fetch_assoc()){
+            echo "<tr><td>".
+            $row["id"]."</td><td>".
+            $row["firstName"]."</td><td>".
+            $row["email"]."</td><td>".
+            $row["birth_date"]."</td><td>".
+            calculateAge($row["birth_date"])."</td><td>".
+            $row["gender"]."</td><td>".
+            $row["description"]."</td><td>".
+            "<a href=\"?user_id=$row[id]\">Szczegóły</a>".
+            "</tr>";
+    }
+    echo "</table>";
+    }else{
+        echo "Brak danych w tabeli użytkownicy";
+    }
+    
+    $mysqli->close();
+?>
+</body>
+</html>
