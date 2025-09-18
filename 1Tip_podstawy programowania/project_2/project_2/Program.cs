1ae7e3a91fac5f8556e784bbb24edb20f08887d2
szyper <31780468+szyper@users.noreply.github.com>
2025-09-18 15:10:09 +0200

    html - tabele

---
+++ 1Tip_podstawy programowania/project_2/project_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("test\n");
+            Console.Write("test");
+            Console.WriteLine("nowa linia");
+            Console.WriteLine("nowa linia\n\n");
+
+            Console.Write("Imię: ");
+            Console.WriteLine("Franek");
+            Console.Write("Wiek: ");
+            Console.WriteLine(20);
+
+            int x = 20;
+            int y = 30;
+
+            Console.Write("Wynik dodawania: ");
+            Console.WriteLine(x + y);
+
+            // komentarz w jednej linii
+
+            /*
+             *
+             *string (tekst)
+             *  char (znak)
+             *  int (liczby całkowite)
+             *  double, float, decimal (liczby zmiennoprzecinkowe)
+             *  false/true (wartości logiczne)
+             * 
+             */
+        }
+    }
+}
