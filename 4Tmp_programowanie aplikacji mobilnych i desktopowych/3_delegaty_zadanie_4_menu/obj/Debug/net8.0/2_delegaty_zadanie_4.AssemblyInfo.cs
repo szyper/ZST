b363397499d908a6eb7f50a4725a17ea60beec68
szyper <31780468+szyper@users.noreply.github.com>
2025-09-18 12:39:58 +0200

    c# - delegaty

--- 4Tmp_programowanie aplikacji mobilnych i desktopowych/3_delegaty_zadanie_4_menu/obj/Debug/net8.0/2_delegaty_zadanie_4.AssemblyInfo.cs
+++ 4Tmp_programowanie aplikacji mobilnych i desktopowych/3_delegaty_zadanie_4_menu/obj/Debug/net8.0/2_delegaty_zadanie_4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2_delegaty_zadanie_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e26339bd7367e225f1f5348747bcfe64de79e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("2_delegaty_zadanie_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2_delegaty_zadanie_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
