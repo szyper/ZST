9a6a2504d6ef172cc9c02490cb6b008e4b72ae3c
szyper <31780468+szyper@users.noreply.github.com>
2025-10-09 14:22:34 +0200

    typy danych

---
+++ 1Tip_podstawy programowania/project_3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklaracja i inicjalizacja zmiennych różnych typów
+            string fullName = "Anna Nowak";
+            char firstLetterOfName = 'A';
+            int age = 30;
+            double heightMeters = 1.65;
+            float bodyTemperature = 36.6f;
+            decimal accountBalance = 2500.75m;
+            bool isStudent = false;
+
+            // wyświetlenie danych
+            Console.WriteLine("Dane osobowe");
+            Console.WriteLine($"Imię i nazwisko (string): {fullName}");
+            Console.WriteLine($"Pierwsza litera imienia (char): {firstLetterOfName}");
+            Console.WriteLine($"Wiek (int): {age} lat");
+            Console.WriteLine($"Wzrost (double): {heightMeters}m");
+            Console.WriteLine($"Temperatura ciała (float): {bodyTemperature} °C");
+            Console.WriteLine($"Saldo konta (decimal): {accountBalance} PLN");
+            Console.WriteLine($"Czy student? (bool): {isStudent}");
+
+            // wykonywanie operacji na zmiennych
+            Console.WriteLine("Wykonywanie operacji na zmiennych");
+            int ageInTwoYears = age + 2;
+            double heightInCm = heightMeters * 100;
+            decimal deposit = 300.45m;
+            decimal newBalance = accountBalance + deposit;
+            bool hasHighTemperature = bodyTemperature >= 37.0f;
+            char nextLetter = (char)(firstLetterOfName + 1); // następna litera w alfabecie
+
+            // interakcja z użytkownikiem
+            Console.Write("\nPodaj liczbę przepracowanych godzin w tym tygodniu: ");
+            string hoursText = Console.ReadLine();
+            if (int.TryParse(hoursText, out int hoursWorked))
+            {
+                decimal hourlyRate = 31.40m;
+                decimal salary = hoursWorked * hourlyRate;
+                Console.WriteLine($"Wynagrodzenie za {hoursWorked} godzin (decimal): {salary} PLN");
+            }
+            else
+            {
+                Console.WriteLine("Błąd: niepoprawny format liczby godzin");
+            }
+
+            Console.WriteLine("\nNaciśnij dowolny klawisz, aby zakończyć...");
+            Console.ReadKey();
+        }
+    }
+}
--- 1Tip_podstawy programowania/project_3/Properties/AssemblyInfo.cs
+++ 1Tip_podstawy programowania/project_3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("5_zdarzenia")]
+[assembly: AssemblyTitle("project_3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("5_zdarzenia")]
+[assembly: AssemblyProduct("project_3")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1595ce8c-2454-48d7-a35d-930ab85eb219")]
+[assembly: Guid("3c3607ee-039e-4e46-bf40-1eebe476afff")]
 
 // Version information for an assembly consists of the following four values:
 //
