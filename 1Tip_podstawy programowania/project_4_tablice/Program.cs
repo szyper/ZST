9cfa972e1fca1335a75506d51c5b05f6b8d49460
szyper <31780468+szyper@users.noreply.github.com>
2025-10-21 08:55:46 +0200

    php

---
+++ 1Tip_podstawy programowania/project_4_tablice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace project_4_tablice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
