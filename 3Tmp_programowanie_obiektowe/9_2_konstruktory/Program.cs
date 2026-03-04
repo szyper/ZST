33061ed96c356ffc0d014e458eb07088cc733aa0
szyper <zskgit@gmail.com>
2026-03-04 13:17:28 +0100

    new files

---
+++ 3Tmp_programowanie_obiektowe/9_2_konstruktory/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace _9_2_konstruktory
+{
+    
+    class Person
+    {
+        // pole przechowujące imię
+        protected string name;
+
+        // konstruktor domyślny
+        public Person()
+        {
+            name = "Brak imienia";
+            Console.WriteLine("1. Konstruktor Person()");
+            Console.WriteLine($"    Ustawiono name = {name}");
+        }
+        
+        // konstrukotr z parametrem
+        // : this oznacza, że najpierw wywołaj Person() a następnie poniższy kod
+        public Person(string name) : this()
+        {
+            // this.name odnosi się do pola klasy
+            // name (bez this) to parametr konstruktora
+            this.name = name;
+            Console.WriteLine("2. Konstruktor Person(string)");
+            Console.WriteLine($"    Zmieniono name na {this.name}");
+        }
+
+    }
+
+    class Student : Person
+    {
+        // pole klasy pochodnej
+        // to pole należy tylko do klasy Student
+        // klasa Person NIE "widzi" tego pola
+
+        private int age;
+
+
+        // konstruktor bezparametrowy
+        // : base("Jan") oznacza, że zanim wykona się ciało tego konstruktora, najpierw zostanie wywołany konstruktor klasy bazowej Person(string) z argumentem "Jan"
+        public Student() : base("Jan")
+        {
+            // w tym momencie:
+            // 1) konstruktor Person() już się wykonał
+            // 2) konstruktor Person(string) już się wykonał
+            // 3) pole name zostało ustawione na "Jan"
+
+            // teraz inicjalizujemy pole klasy Student
+            age = 0;
+            Console.WriteLine("3. Konstruktor Student()");
+            Console.WriteLine($"    Student: name = {name}, age = {age}");
+        }
+
+        // konstruktor z parametrem
+        // : this() oznacza, że najpierw wywołaj konstruktor Student() a dopiero później wykonaj poniższy kod
+        public Student(int age) : this()
+        {
+            // w tym momencie:
+            // 1) Student() już się wykonał
+            // 2) base("Jan") też już się wykonał
+            // 3) pole name = "Jan"
+            // 4) pole age = 0
+
+            // this.age -> pole klasy
+            // age -> parametr konstruktora
+            // nadpisujemy wcześniejszą wartość (0)
+            
+            this.age = age;
+            Console.WriteLine("4. Konstruktor Student(int)");
+            Console.WriteLine($"    Student: name = {name}, age = {this.age}");
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Person p = new Person();
+            // Person p = new Person("Franuś");
+            // Student s = new Student();
+            Student s = new Student(25);
+        }
+    }
+}
---
+++ 3Tmp_programowanie_obiektowe/9_3_konstruktor_statyczny/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Net.NetworkInformation;
+
+namespace _9_3_konstruktor_statyczny
+{
+    class Car
+    {
+        // pole statyczne - wspólne dla wszystkich obiektów, liczy ile samochodów utworzono
+        public static int Counter;
+
+        // pole statyczne - maksymalna liczba obiektów
+        public static int MaxCars;
+
+        // pole statyczne - kolejny dostępny ID
+        public static int NextId;
+
+        // pola instancyjne - unikalne dla każdego obiektu
+        public string Brand;
+        public int Year;
+        public int Id;
+
+        // konstruktor statyczny
+        //nie tworzy obiektów, działa na klasie jako całości, nie przyjmuje parametrów, wykonuje się tylko raz, przed pierwszym użyciem klasy
+        static Car()
+        {
+            Counter = 0;
+            MaxCars = 3;
+            NextId = 1;
+            Console.WriteLine("Konstruktor statyczny Car()");
+            Console.WriteLine($"Maksymalna liczba samochodów: {MaxCars}");
+        }
+
+        // konstruktor instancyjny, to konstruktor, który tworzy obiekt klasy (czyli instancję). Może być domyślny (bez parametrów) lub parametryczny (z parametrami). Nie jest statyczny - wykonuje siędla konkretnego obiektu
+
+        // konstruktor instancyjny domyślny
+        public Car()
+        {
+            checkLimit("domyślnego");
+            Brand = "nieznana";
+            Year = 0;
+            Id = NextId++;
+
+            Counter++;
+            Console.WriteLine($"Car(): Id = {Id}, marka: {Brand}, rok produkcji: {Year}, ilość samochodów: {Counter}");
+        }
+
+        // konstruktor instancyjny parametryczny
+        public Car(string brand, int year)
+        {
+            checkLimit(brand);
+            Brand = brand;
+            Year = year;
+            Id = NextId++;
+
+            Counter++; // zliczamy obiekty
+            Console.WriteLine($"Car(): Marka: {Brand}, rok produkcji: {Year}, licznik: {Counter}");
+        }
+
+        private void checkLimit(string name)
+        {
+            if (Counter >= MaxCars)
+                throw new InvalidOperationException($"Nie można utworzyć samochodu {name}, osiągnięto limit {MaxCars}");
+        }
+
+        // metoda instancyjna - wypisuje dane samochodu
+        public void Print()
+        {
+            Console.WriteLine($"Samochód ID {Id}: marka: {Brand}, rok produkcji: {Year}");
+        }
+
+        // metoda statyczna - wypisuje liczbę stworzonych samochodów
+        public static void ShowCounter()
+        {
+            Console.WriteLine($"\nLiczba utworzonych samochodów: {Counter}\n");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // pierwsze użycie klasy -> wywołuje się konstruktor statyczny
+            // Próba utworzenia 4 samochodów przy limicie 3
+
+            Car.ShowCounter();
+
+            Car car1 = null;
+            Car car2 = null;
+            Car car3 = null;
+            Car car4 = null;
+
+            try
+            {
+                car1 = new Car();
+                car2 = new Car("Toyota", 2022);
+                car3 = new Car("Opel", 2000);
+                car4 = new Car();
+
+            }
+            catch (InvalidOperationException ex) 
+            {
+                Console.WriteLine($"Błąd: {ex.Message}");
+            }
+
+            if (car1 != null) car1.Print();
+            if (car2 != null) car2.Print();
+            if (car3 != null) car3.Print();
+            if (car4 != null) car4.Print();
+
+            Car.ShowCounter();
+
+        }
+    }
+}
