752f2920afc27a582b87f418f08fd89a63ab8b2d
szyper <zskgit@gmail.com>
2026-03-26 13:51:02 +0100

    new files

---
+++ 3Tmp_programowanie_obiektowe/10_1_dziedziczenie_zapis_w_pliku/Program.cs
@@ -0,0 +1,331 @@
+﻿
+
+
+namespace _10_dziedziczenie
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    public class Author : Person
+    {
+        // public List<Book> BooksList { get; set; } = new List<Book>();
+        public List<Book> BooksList { get; set; }
+
+        public Author(string firstName, string lastName) : base(firstName, lastName) 
+        {
+            BooksList = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            BooksList.Add(book);
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public int PublicationYear { get; set; }
+
+        public Book(string title, Author author, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+        }
+    }
+
+    public class Reader : Person
+    {
+        public List<Book> BorrowedBooksList { get; set; }
+        public Reader(string firstName, string lastName) : base(firstName, lastName)
+        {
+            BorrowedBooksList = new List<Book>();
+        }
+
+        public void BorrowBook(Book book)
+        {
+            BorrowedBooksList.Add(book);
+            Console.WriteLine($"Czytelnik {FirstName} {LastName} wypożyczył książkę: {book.Title}");
+        }
+    }
+
+    public class Library
+    {
+        public List<Book> BooksList { get; set; }
+        public List<Reader> ReadersList { get; set; }
+        public List<Author> AuthorList { get; set; }
+
+        public Library()
+        {
+            BooksList = new List<Book>();
+            ReadersList = new List<Reader>();
+            AuthorList = new List<Author>();
+        }
+
+        public void AddBook(Book book)
+        {
+            BooksList.Add(book);
+            Console.WriteLine($"Dodano książkę: {book.Title}");
+        }
+
+        public void AddReader(Reader reader)
+        {
+            ReadersList.Add(reader);
+            Console.WriteLine($"Dodano czytelnika: {reader.FirstName} {reader.LastName}");
+        }
+
+        public void AddAuthor(Author author)
+        {
+            AuthorList.Add(author);
+        }
+
+        public void BorrowBook(Reader reader, Book book)
+        {
+            if (BooksList.Contains(book))
+            {
+                reader.BorrowBook(book);
+                BooksList.Remove(book);
+                Console.WriteLine($"Książka {book.Title} została wypożyczona przez {reader.FirstName} {reader.LastName}");
+            }
+            else
+            {
+                Console.WriteLine($"Książka {book.Title} nie jest dostępna w bibliotece");
+            }
+        }
+
+        public void DisplayBorrowedBooks()
+        {
+            Console.WriteLine("Wypożyczone książki:");
+            foreach (var reader in ReadersList)
+            {
+                foreach (var book in reader.BorrowedBooksList)
+                {
+                    Console.WriteLine($"{book.Title} - {book.Author.FirstName} {book.Author.LastName} ({book.PublicationYear}) wypożyczona przez {reader.FirstName} {reader.LastName}");
+                }
+            }
+        }
+
+        internal void DisplayAuthorsTable()
+        {
+            if (AuthorList.Count == 0)
+            {
+                Console.WriteLine("Brak książek w bibliotece");
+                return;
+            }
+
+            Console.WriteLine("Lista autorów:");
+            // Console.WriteLine("{0, -3} {1, -13} {2, -13}", "ID", "Imię", "Nazwisko");
+            Console.WriteLine("+-----+---------------+---------------+");
+            Console.WriteLine($"| { "ID", -3 } | { "Imię", -13} | { "Nazwisko", -13} |");
+            Console.WriteLine("+-----+---------------+---------------+");
+
+            for (int i = 0; i < AuthorList.Count; i++)
+            {
+                Console.WriteLine($"| {i + 1, -3} | {AuthorList[i].FirstName, -13} | {AuthorList[i].LastName, -13} |");
+            }
+            Console.WriteLine("+-----+---------------+---------------+");
+        }
+
+        internal void DisplayBooks()
+        {
+            if (BooksList.Count == 0)
+            {
+                Console.WriteLine("Brak książek w bibliotece");
+                return;
+            }
+
+            Console.WriteLine("Książki w bibliotece:");
+            const int titleWidth = 30;
+            const int authorWidth = 25;
+            const int yearWidth = 6;
+
+            string line = "+" + new string('-', titleWidth + 2) + "+" + new string('-', authorWidth + 2) + "+" + new string('-',yearWidth + 2) + "+";
+
+            Console.WriteLine(line);
+            Console.WriteLine($"| {"Tytuł",-titleWidth} | {"Autor",-authorWidth} | {"Rok",-yearWidth} |");
+
+            foreach (var book in BooksList)
+            {
+                string authorFullName = $"{book.Author.FirstName} {book.Author.LastName}";
+                Console.WriteLine($"| {book.Title, -titleWidth} | {authorFullName, -authorWidth} | {book.PublicationYear, -yearWidth} |");
+            }
+        }
+
+        internal void ExportLibrary(string? folderPath)
+        {
+            if (!Directory.Exists(folderPath)) 
+                Directory.CreateDirectory(folderPath); 
+                
+            using(var writer = new StreamWriter(Path.Combine(folderPath, "authors.csv")))
+            {
+                writer.WriteLine("FirstName, LastName");
+
+                foreach (var author in AuthorList)
+                    writer.WriteLine($"{author.FirstName},{author.LastName}");
+            }
+        }
+
+        internal void ImportLibrary(string? folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                Console.WriteLine("Folder nie istnieje!");
+                return;
+            }
+
+            string authorsPath = Path.Combine(folderPath, "authors.csv");
+
+            if (File.Exists(authorsPath))
+            {
+                var lines = File.ReadAllLines(authorsPath);
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    var parts = lines[i].Split(',');
+                    if (parts.Length != 2) continue;
+                    if (AuthorList.Find(a => a.FirstName == parts[0] && a.LastName == parts[1]) == null)
+                        AuthorList.Add(new Author(parts[0], parts[1]));
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        // zadanie_1 biblioteka
+        static void Main(string[] args)
+        {
+            //Author author = new Author("Adam", "Mickiewicz");
+            //Book book = new Book("Pan Tadeusz", author, 1834);
+            //author.AddBook(book);
+
+            //Reader reader = new Reader("Jan", "Kowalski");
+            Library library = new Library();
+            //library.AddBook(book);
+            //library.AddReader(reader);
+            //library.BorrowBook(reader, book);
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Dodaj autora");
+                Console.WriteLine("2. Dodaj książkę");
+                Console.WriteLine("3. Dodaj czytelnika");
+                Console.WriteLine("4. Wypożycz książkę");
+                Console.WriteLine("5. Wyświetl wszystkie książki");
+                Console.WriteLine("6. Wyświetl wszystkich autorów");
+                Console.WriteLine("7. Wyświetl wszystkie wypożyczone książki");
+                Console.WriteLine("8. Eksportuj bibliotekę do CSV");
+                Console.WriteLine("9. Importuj bibliotekę z CSV");
+                Console.WriteLine("10. Wyjście");
+                Console.Write("Wybierz opcję: ");
+
+                string choice = Console.ReadLine();
+                switch (choice) 
+                {
+                    case "1":
+                        Console.Write("Podaj imię autora:");
+                        string authorFirstName = Console.ReadLine();
+                        Console.Write("Podaj nazwisko autora:");
+                        string authorLastName = Console.ReadLine();
+                        library.AddAuthor(new Author(authorFirstName, authorLastName));
+                        break;
+                    case "2":
+                        if (library.AuthorList.Count == 0)
+                        {
+                            Console.WriteLine("Brak autorów w bibliotece. Najpierw dodaj autora!");
+                            Console.WriteLine("Naciśnij dowolny klawisz, aby kontynuować...");
+                            Console.ReadKey();
+                            break;
+                        }
+                        library.DisplayAuthorsTable();
+                        Console.Write("Podaj ID autora: ");
+                        int authorIndex = int.Parse(Console.ReadLine()) - 1;
+                        if (authorIndex >= 0 && authorIndex < library.AuthorList.Count)
+                        {
+                            Author selectedAuthor = library.AuthorList[authorIndex];
+                            Console.Write("Podaj tytuł książki: ");
+                            string bookTitle = Console.ReadLine();
+                            Console.Write("Podaj rok publikacji: ");
+                            int publicationYear = int.Parse(Console.ReadLine());
+                            Book newBook = new Book(bookTitle, selectedAuthor, publicationYear);
+                            library.AddBook(newBook);
+                            selectedAuthor.AddBook(newBook);
+                        } 
+                        else
+                        {
+                            Console.WriteLine("Nieprawidłowy numer autora");
+                        }
+                        break;
+                    case "3":
+                        Console.Write("Podaj imię czytelnika:");
+                        string readerFirstName = Console.ReadLine();
+                        Console.Write("Podaj nazwisko czytelnika:");
+                        string readerLastName = Console.ReadLine();
+                        library.AddReader(new Reader(readerFirstName, readerLastName));
+                        break;
+                    case "4":
+                        Console.Write("Podaj imię czytelnika: ");
+                        string borrowerFirstName = Console.ReadLine();
+                        Console.Write("Podaj nazwisko czytelnika: ");
+                        string borrowerLastName = Console.ReadLine();
+                        Reader borrower = library.ReadersList.Find(r => r.FirstName == borrowerFirstName && r.LastName == borrowerLastName);
+
+                        if (borrower != null)
+                        {
+                            Console.Write("Podaj tytuł książki: ");
+                            string borrowedBookTitle = Console.ReadLine();
+                            Book borrowedBook = library.BooksList.Find(b => b.Title == borrowedBookTitle);
+                            if (borrowedBook != null)
+                            {
+                                library.BorrowBook(borrower, borrowedBook);
+                            } else
+                            {
+                                Console.WriteLine("Książka nie jest dotępna");
+                            }
+                        } else
+                        {
+                            Console.WriteLine("Czytelnik nie został znaleziony");
+                        }
+                        break;
+                    case "5":
+                        library.DisplayBooks();
+                        break;
+                    case "6":
+                        library.DisplayAuthorsTable();
+                        break;
+                    case "7":
+                        library.DisplayBorrowedBooks();
+                        break;
+                    case "8":
+                        Console.Write("Podaj folder do zapisania CSV: ");
+                        string exportFolder = Console.ReadLine();
+                        library.ExportLibrary(exportFolder);
+                        break;
+                    case "9":
+                        Console.Write("Podaj folder z plikami CSV: ");
+                        string importFolder = Console.ReadLine();
+                        library.ImportLibrary(importFolder);
+                        break;
+                    case "10":
+                        exit = true;
+                        break;
+                }
+                Console.WriteLine("\n\nNaciśnij dowolny klawisz, aby kontynuować...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 4Tmp_programowanie aplikacji mobilnych i desktopowych/egzaminy_konsola/5_pesel/Program.cs
@@ -0,0 +1,90 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+namespace _4_pesel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Sprawdzenie numeru PESEL ===");
+            Console.Write("Podaj numer PESEL (11 cyfr): ");
+            string pesel = Console.ReadLine();
+
+            if (pesel.Length != 11 || !IsDigitsOnly(pesel))
+            {
+                Console.WriteLine("Błąd: PESEL musi zawierać dokładnie 11 cyfr!");
+                return;
+            }
+
+            char gender = GetGender(pesel);
+
+            Console.Write("Płeć: ");
+            if (gender == 'K')
+                Console.WriteLine("Kobieta");
+            else
+                Console.WriteLine("Mężczyzna");
+
+            bool isValid = CheckPesel(pesel);
+
+            if (isValid)
+                Console.WriteLine("PESEL jest POPRAWNY (zgodna suma kontrolna)");
+            else
+                Console.WriteLine("PESEL jest NIEPOPRAWNY (błędna suma kontrolna)");
+        }
+
+        /*
+         ********************************* 
+         nazwa funkcji: CheckPesel
+         opis funkcji: sprawdza poprawność numeru PESEL na podstawie sumy kontrolnej
+         parametry: pesel - łańcuch znaków zawierający numer PESEL
+         zwracany typ i opis: bool - zwraca true, gdy PESEL jest poprawny, w przeciwnym razie false
+         autor: 12345678901
+         *********************************        */
+        private static bool CheckPesel(string pesel)
+        {
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+
+            int S = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int digit = int.Parse(pesel[i].ToString());
+                S += digit * weights[i];
+            }
+
+            int M = S % 10;
+
+            int R;
+
+            if (M == 0)
+                R = 0;
+            else
+                R = 10 - M;
+
+            int controlDigit = int.Parse(pesel[10].ToString());
+
+            return R == controlDigit; 
+        }
+
+        private static char GetGender(string pesel)
+        {
+            int digit = int.Parse(pesel[9].ToString());
+
+            if (digit % 2 == 0)
+                return 'K';
+            else
+                return 'M';
+        }
+
+        private static bool IsDigitsOnly(string text)
+        {
+            foreach (char c in text)
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
